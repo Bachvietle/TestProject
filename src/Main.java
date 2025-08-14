56152bffb2237fcf918d9b6d022b85a1701d5ea5
Viet Bach <vietbbachh@gmail.com>
2025-08-14 16:47:50 +0700

    Thêm cat, update main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Dog dog1 = new Dog(4, 6);
 
-        System.out.println(dog1.age);
         System.out.println(dog1.sochan);
     }
 }
