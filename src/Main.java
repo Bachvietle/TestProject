552e295ff7edefa23f538459cad7aa31e46b0e81
Viet Bach <vietbbachh@gmail.com>
2025-08-14 17:16:27 +0700

    NT

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Dog dog1 = new Dog(4, 6);
+        Dog dog1 = new Dog(4, 6, "Bon");
 
         System.out.println(dog1.sochan);
     }
