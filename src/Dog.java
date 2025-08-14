b185060a5ffd56ab732be2f4e11617e33227c6e3
Viet Bach <vietbbachh@gmail.com>
2025-08-14 16:18:18 +0700

    Thêm Dog class, thêm main

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal{
+
+    int age;
+
+    public Dog(int sochan, int age){
+        super(sochan);
+
+        this.age = age;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog(4, 6);
+
+        System.out.println(dog1.age);
+    }
+}
